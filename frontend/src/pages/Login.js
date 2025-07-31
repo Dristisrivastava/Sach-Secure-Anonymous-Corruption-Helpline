86ac16d65dedb0c8fdd83a53d584a19e3a01815a
Dristi srivastava <113194837+Dristisrivastava@users.noreply.github.com>
2025-07-31 10:54:18 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -9,7 +9,7 @@ export default function Login({ setUser }) {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
         username,
         password,
       });
